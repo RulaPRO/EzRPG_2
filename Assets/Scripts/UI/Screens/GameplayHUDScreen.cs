dbce0d9cffbadbced6bfde0dcc71f272e83610d7
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-07-29 06:52:04 +0300

    Refactor GameplayHUDScreen

--- Assets/Scripts/UI/Screens/GameplayHUDScreen.cs
+++ Assets/Scripts/UI/Screens/GameplayHUDScreen.cs
@@ -1,6 +1,8 @@
-﻿using Core.Services.Interfaces;
+﻿using Commands;
+using Core.CommandRunner.Interfaces;
 using Core.Services.UI;
 using UI.Popups;
+using UI.Widgets;
 using UnityEngine;
 using UnityEngine.UI;
 using VContainer;
@@ -11,19 +13,34 @@ namespace UI.Screens
     {
         [SerializeField] private Button button;
 
-        private IUIService uiService;
-
-        [Inject]
-        public void Construct(IUIService uiService)
-        {
-            this.uiService = uiService;
-
-            Debug.Log($"GameplayHUDScreen Construct. Inject - {uiService.GetType()}");
-        }
+        private ICommandExecutionService commandExecutionService;
 
         private void Start()
         {
-            button.onClick.AddListener(() => uiService.ShowPopup<ConfirmationPopup>());
+            button.onClick.AddListener(() =>
+            {
+                commandExecutionService.Execute<ShowPopupCommand<ConfirmationPopup>>();
+            });
+        }
+
+        [Inject]
+        public void Construct(ICommandExecutionService commandExecutionService)
+        {
+            this.commandExecutionService = commandExecutionService;
+        }
+
+        public override void Show()
+        {
+            base.Show();
+
+            commandExecutionService.Execute<ShowWidgetCommand<TopBarWidget>>();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+
+            commandExecutionService.Execute<ShowWidgetCommand<TopBarWidget>>();
         }
     }
 }
