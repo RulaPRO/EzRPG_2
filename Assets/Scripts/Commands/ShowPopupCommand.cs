667a09cd0c59039f2a535a674f37e82eba42c804
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-07-29 06:37:47 +0300

    ShowPopupCommand

---
+++ Assets/Scripts/Commands/ShowPopupCommand.cs
@@ -0,0 +1,24 @@
+﻿using Core.CommandRunner.Interfaces.Command;
+using Core.Services.Interfaces;
+using Core.Services.UI;
+using VContainer;
+
+namespace Commands
+{
+    public class ShowPopupCommand<TPopup> : ICommand
+        where TPopup : UIPopup
+    {
+        private IUIService uiService;
+
+        [Inject]
+        public void Construct(IUIService uiService)
+        {
+            this.uiService = uiService;
+        }
+
+        public void Execute()
+        {
+            uiService.ShowPopup<TPopup>();
+        }
+    }
+}
