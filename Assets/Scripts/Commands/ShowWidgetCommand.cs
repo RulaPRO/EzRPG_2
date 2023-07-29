0c75c507307bdea56986b53fa043e1de08b00df4
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-07-29 06:42:17 +0300

    ShowWidgetCommand

---
+++ Assets/Scripts/Commands/ShowWidgetCommand.cs
@@ -0,0 +1,24 @@
+﻿using Core.CommandRunner.Interfaces.Command;
+using Core.Services.Interfaces;
+using Core.Services.UI;
+using VContainer;
+
+namespace Commands
+{
+    public class ShowWidgetCommand<TWidget> : ICommand
+        where TWidget : UIWidget
+    {
+        private IUIService uiService;
+
+        [Inject]
+        public void Construct(IUIService uiService)
+        {
+            this.uiService = uiService;
+        }
+
+        public void Execute()
+        {
+            uiService.ShowWidget<TWidget>();
+        }
+    }
+}
