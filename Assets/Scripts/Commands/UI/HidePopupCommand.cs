2780a4429adc1716b69f3030e73397aea4ebd2b1
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-08-19 15:57:04 +0300

    Refactor

---
+++ Assets/Scripts/Commands/UI/HidePopupCommand.cs
@@ -0,0 +1,24 @@
+﻿using Core.CommandRunner.Interfaces.Command;
+using Core.Services.Interfaces;
+using Core.Services.UI;
+using VContainer;
+
+namespace Commands
+{
+    public class HidePopupCommand<TPopup> : ICommand
+        where TPopup : UIPopup
+    {
+        private IUIService uiService;
+
+        [Inject]
+        public void Construct(IUIService uiService)
+        {
+            this.uiService = uiService;
+        }
+
+        public void Execute()
+        {
+            uiService.HidePopup<TPopup>();
+        }
+    }
+}
