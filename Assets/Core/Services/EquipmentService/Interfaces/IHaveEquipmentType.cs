1239a663648da2894a6595f46b8c46a98d9a21d5
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-11 14:20:34 +0300

    Update EquipmentType logic

---
+++ Assets/Core/Services/EquipmentService/Interfaces/IHaveEquipmentType.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Services.EquipmentService.Interfaces
+{
+    public interface IHaveEquipmentType
+    {
+        EquipmentType EquipmentType { get; }
+    }
+
+    public enum EquipmentType
+    {
+        Unknown = 0,
+        Weapon = 1,
+        Helmet = 2,
+        Gloves = 3,
+        Torso = 4,
+        Pants = 5,
+        Boots = 6
+    }
+}
