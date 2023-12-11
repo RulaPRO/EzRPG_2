0ae258f8c9368b170fa22cd7a9442c014c060151
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-11 14:26:23 +0300

    Implement RarityType

---
+++ Assets/Core/Services/InventoryService/Interfaces/IHaveRarityType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Core.Services.InventoryService.Interfaces
+{
+    public interface IHaveRarityType
+    {
+        RarityType RarityType { get; }
+        event Action OnRarityChanged;
+    }
+
+    public enum RarityType
+    {
+        Unknown = 0,
+        Common = 1,
+        Uncommon = 2,
+        Rare = 3,
+        Epic = 4,
+        Legendary = 5,
+        Mythical = 6
+    }
+}
