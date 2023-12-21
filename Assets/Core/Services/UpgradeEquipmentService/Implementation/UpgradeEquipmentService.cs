0fb1e5b4e6a6ac4fa3cc9aba8bd28da0f252264c
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-21 16:34:41 +0300

    IUpgradeEquipmentService prototype

---
+++ Assets/Core/Services/UpgradeEquipmentService/Implementation/UpgradeEquipmentService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Services.EquipmentService.Implementation;
+using Core.Services.EquipmentService.Interfaces;
+using Core.Services.InventoryService.Interfaces;
+using Core.Services.UpgradeEquipmentService.Interfaces;
+
+namespace Core.Services.UpgradeEquipmentService.Implementation
+{
+    public class UpgradeEquipmentService : IUpgradeEquipmentService
+    {
+        public IEquipmentItem UpgradeEquipmentRarity(IEquipmentItem equipmentItem)
+        {
+            var nextRarity = (RarityType)((int)equipmentItem.RarityType + 1);
+            var upgradedItem = new EquipmentItem(equipmentItem.ObjectId, equipmentItem.EquipmentType, nextRarity);
+
+            return upgradedItem;
+        }
+    }
+}
---
+++ Assets/Core/Services/UpgradeEquipmentService/Interfaces/IUpgradeEquipmentService.cs
@@ -0,0 +1,9 @@
+﻿using Core.Services.EquipmentService.Interfaces;
+
+namespace Core.Services.UpgradeEquipmentService.Interfaces
+{
+    public interface IUpgradeEquipmentService
+    {
+        IEquipmentItem UpgradeEquipmentRarity(IEquipmentItem equipmentItem);
+    }
+}
