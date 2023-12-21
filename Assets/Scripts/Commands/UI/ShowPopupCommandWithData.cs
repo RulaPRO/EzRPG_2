713939fc3f6c690a2167b91c22d8c9f50cd08b2f
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-21 19:46:07 +0300

    ShowPopupCommandWithData

---
+++ Assets/Scripts/Commands/UI/ShowPopupCommandWithData.cs
@@ -0,0 +1,28 @@
+﻿using Core.CommandRunner.Interfaces.Command;
+using Core.Services.Interfaces;
+using Core.Services.UI;
+using VContainer;
+
+namespace Commands.UI
+{
+    public abstract class ShowPopupCommandWithData<TPopup, TData> : ICommandWithData<TData>
+    where TPopup : UIPopup
+    where TData : struct
+    {
+        private IUIService uiService;
+
+        [Inject]
+        public void Construct(IUIService uiService)
+        {
+            this.uiService = uiService;
+        }
+
+        public virtual void Execute(TData commandData)
+        {
+            var popup = uiService.ShowPopup<TPopup>();
+            InitializePopup(popup, commandData);
+        }
+
+        protected abstract void InitializePopup(TPopup popup, TData data);
+    }
+}
