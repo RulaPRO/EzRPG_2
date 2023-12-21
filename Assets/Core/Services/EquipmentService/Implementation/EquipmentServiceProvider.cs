40dc8d2ac2222af6115b88a30a3330794f99dced
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-21 16:29:43 +0300

    IEquipmentServiceProvider

---
+++ Assets/Core/Services/EquipmentService/Implementation/EquipmentServiceProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Services.EquipmentService.Interfaces;
+using VContainer;
+
+namespace Core.Services.EquipmentService.Implementation
+{
+    public class EquipmentServiceProvider : IEquipmentServiceProvider, IDisposable
+    {
+        public event Action<IEquipmentItem> OnItemAdded;
+        public event Action<IEquipmentItem> OnItemRemoved;
+        public event Action<string> OnItemRarityUpgraded;
+        public IReadOnlyDictionary<string, IEquipmentItem> AvailableItems => equipmentService.AvailableItems;
+
+        private readonly IEquipmentService equipmentService;
+
+        [Inject]
+        public EquipmentServiceProvider(
+            IEquipmentService equipmentService)
+        {
+            this.equipmentService = equipmentService;
+
+            equipmentService.OnItemAdded += OnEquipmentItemAdd;
+            equipmentService.OnItemRemoved += OnEquipmentItemRemove;
+            equipmentService.OnItemRarityUpgraded += OnEquipmentItemRarityUpgrade;
+        }
+
+        public void Dispose()
+        {
+            equipmentService.OnItemAdded -= OnEquipmentItemAdd;
+            equipmentService.OnItemRemoved -= OnEquipmentItemRemove;
+            equipmentService.OnItemRarityUpgraded -= OnItemRarityUpgraded;
+        }
+
+        private void OnEquipmentItemAdd(string id)
+        {
+            OnItemRarityUpgraded?.Invoke(id);
+        }
+
+        private void OnEquipmentItemRemove(string id)
+        {
+            OnItemRarityUpgraded?.Invoke(id);
+        }
+
+        private void OnEquipmentItemRarityUpgrade(string id)
+        {
+            OnItemRarityUpgraded?.Invoke(id);
+        }
+    }
+}
---
+++ Assets/Core/Services/EquipmentService/Interfaces/IEquipmentServiceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Services.EquipmentService.Interfaces
+{
+    public interface IEquipmentServiceProvider
+    {
+        event Action<IEquipmentItem> OnItemAdded;
+        event Action<IEquipmentItem> OnItemRemoved;
+        event Action<string> OnItemRarityUpgraded;
+        IReadOnlyDictionary<string, IEquipmentItem> AvailableItems { get; }
+    }
+}
