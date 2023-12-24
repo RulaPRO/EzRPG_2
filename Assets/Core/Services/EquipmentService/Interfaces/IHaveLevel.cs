d165b561d3d47868e8e575dc4e68515bcf6986bc
Hetmanchyk Ruslan <ruslan.hetmanchyk@eon-games.com>
2023-12-24 17:17:36 +0300

    IHaveLevel

---
+++ Assets/Core/Services/EquipmentService/Interfaces/IHaveLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Services.EquipmentService.Interfaces
+{
+    public interface IHaveLevel
+    {
+        int CurrentLevel { get; }
+        int MaxLevel { get; }
+        bool IsLevelMax { get; }
+    }
+}
