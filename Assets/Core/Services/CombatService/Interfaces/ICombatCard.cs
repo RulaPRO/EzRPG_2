3e266d96d5eb16c7bc8fc0c4050762c6cbee3ea5
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-10 17:05:30 +0300

    ICombatCard

---
+++ Assets/Core/Services/CombatService/Interfaces/ICombatCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Services.CardDeckService.Interfaces;
+
+namespace Core.Services.CombatService.Interfaces
+{
+    public interface ICombatCard
+    {
+        event Action OnCurrentPowerChange;
+
+        ICard Card { get; }
+        int CurrentCardPower { get; }
+        Dictionary<CardBonusType, int> Bonuses { get; }
+
+        void ApplyDamage(int value);
+    }
+}
