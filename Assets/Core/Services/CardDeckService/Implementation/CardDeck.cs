09cde3557673ecb543e236b28dddf60942757b25
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-10 16:53:51 +0300

    Implement ICardDeck

---
+++ Assets/Core/Services/CardDeckService/Implementation/CardDeck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Core.Services.CardDeckService.Interfaces;
+
+namespace Core.Services.CardDeckService.Implementation
+{
+    public class CardDeck : ICardDeck
+    {
+        public IReadOnlyDictionary<string, ICard> Cards => cards;
+        private Dictionary<string, ICard> cards = new();
+        public string SelectedCardId { get; set; }
+
+        public void AddCard(string id, ICard card)
+        {
+            cards.Add(id, card);
+        }
+
+        public void RemoveCard(string id)
+        {
+            cards.Remove(id);
+        }
+
+        public void SwapCard(string id, ICard card)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
