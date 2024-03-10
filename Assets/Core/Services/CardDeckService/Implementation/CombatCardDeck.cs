4d1a3a182855afa4ff1f2cfd8d7a4ad61434b51c
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-10 17:31:34 +0300

    CombatCardDeck

---
+++ Assets/Core/Services/CardDeckService/Implementation/CombatCardDeck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Services.CardDeckService.Interfaces;
+using Core.Services.CombatService.Implementation;
+
+namespace Core.Services.CardDeckService.Implementation
+{
+    public class CombatCardDeck
+    {
+        public Dictionary<string, CombatCard> Cards = new();
+        public CombatCard SelectedCard => Cards[SelectedCardId];
+        public string SelectedCardId { get; set; }
+
+        public CombatCardDeck(ICardDeck cardDeck)
+        {
+            foreach (var pair in cardDeck.Cards)
+            {
+                Cards.Add(pair.Key, new CombatCard(pair.Value));
+            }
+        }
+
+        public bool IsAnyAgroCardActive()
+        {
+            return Cards.Values.Any(card => card.Bonuses.ContainsKey(CardBonusType.AGRO));
+        }
+    }
+}
