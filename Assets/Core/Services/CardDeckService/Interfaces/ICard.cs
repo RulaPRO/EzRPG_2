b46baba0f78fa82f6cefa73a6202d00c946a7cc6
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-10 16:50:06 +0300

    ICard

---
+++ Assets/Core/Services/CardDeckService/Interfaces/ICard.cs
@@ -0,0 +1,11 @@
+﻿using Balance;
+using Core.Services.EquipmentService.Interfaces;
+using Core.Services.InventoryService.Interfaces;
+
+namespace Core.Services.CardDeckService.Interfaces
+{
+    public interface ICard : IHaveObjectId, IHaveRarityType, IHaveLevel, IHavePower, IHaveAttackType
+    {
+        CardConfig Config { get; }
+    }
+}
