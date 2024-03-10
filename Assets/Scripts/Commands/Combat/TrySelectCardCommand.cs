56f59f3d75ac6e9d0f9781772a5a295feb8c4786
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2024-03-10 17:43:30 +0300

    TrySelectCardCommand

---
+++ Assets/Scripts/Commands/Combat/TrySelectCardCommand.cs
@@ -0,0 +1,17 @@
+﻿using Core.CommandRunner.Interfaces.Command;
+using Core.Services.CombatService.Interfaces;
+using VContainer;
+
+namespace Commands.Combat
+{
+    public class TrySelectCardCommand : ICommandWithData<SelectCardData>
+    {
+        [Inject]
+        public ICombatService CombatService { get; private set; }
+
+        public void Execute(SelectCardData commandData)
+        {
+            CombatService.TrySelectCard(commandData.CardId);
+        }
+    }
+}
